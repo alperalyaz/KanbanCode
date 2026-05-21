9d8f7ab240c55d643bad046a5996ce272894a24e
777genius <quantjumppro@gmail.com>
2026-05-21 12:57:29 +0300

    fix(linux): install cli launcher in packages

--- resources/afterInstall.sh
+++ resources/afterInstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
+
 # Fix chrome-sandbox permissions for SUID sandbox on Linux
 # See: https://github.com/electron/electron/issues/17972
 
-SANDBOX_PATH="/opt/${productFilename}/chrome-sandbox"
+SANDBOX_PATH="/opt/${sanitizedProductName}/chrome-sandbox"
 
 if [ -f "$SANDBOX_PATH" ]; then
   chown root:root "$SANDBOX_PATH"
