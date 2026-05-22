25f1df02da62014ca478c2d3471662adcb78855b
777genius <quantjumppro@gmail.com>
2026-05-22 10:48:08 +0300

    refactor(team): extract snapshot structural sharing

--- src/renderer/store/slices/teamSlice.ts
+++ src/renderer/store/slices/teamSlice.ts
@@ -91,6 +91,7 @@ import {
   collectTeamScopedStateRemovals,
   collectTeamScopedVisibleLoadingResets,
 } from '../team/teamScopedStateCleanup';
+import { structurallyShareTeamSnapshot } from '../team/teamSnapshotStructuralSharing';
 import { noteTeamRefreshFanout } from '../teamRefreshFanoutDiagnostics';
 import { getWorktreeNavigationState } from '../utils/stateResetHelpers';
 
@@ -530,66 +531,6 @@ function nowIso(): string {
 
 function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function isPlainObject(value: unknown): value is Record<string, unknown> {
-  if (value == null || typeof value !== 'object') {
-    return false;
-  }
-  const prototype = Object.getPrototypeOf(value);
-  return prototype === Object.prototype || prototype === null;
-}
-
-function structurallySharePlainValue<T>(previous: T, next: T): T {
-  if (Object.is(previous, next)) {
-    return previous;
-  }
-
-  if (Array.isArray(previous) && Array.isArray(next)) {
-    let changed = previous.length !== next.length;
-    const result = next.map((nextItem, index) => {
-      const sharedItem = structurallySharePlainValue(previous[index], nextItem);
-      if (!Object.is(sharedItem, previous[index])) {
-        changed = true;
-      }
-      return sharedItem;
-    });
-    return changed ? (result as T) : previous;
-  }
-
-  if (isPlainObject(previous) && isPlainObject(next)) {
-    const previousRecord = previous as Record<string, unknown>;
-    const nextRecord = next as Record<string, unknown>;
-    const previousKeys = Object.keys(previousRecord);
-    const nextKeys = Object.keys(nextRecord);
-    let changed = previousKeys.length !== nextKeys.length;
-    const result: Record<string, unknown> = {};
-
-    for (const key of nextKeys) {
-      if (!Object.prototype.hasOwnProperty.call(previousRecord, key)) {
-        changed = true;
-      }
-      const sharedValue = structurallySharePlainValue(previousRecord[key], nextRecord[key]);
-      if (!Object.is(sharedValue, previousRecord[key])) {
-        changed = true;
-      }
-      result[key] = sharedValue;
-    }
-
-    return changed ? (result as T) : previous;
-  }
-
-  return next;
-}
-
-function structurallyShareTeamSnapshot(
-  previous: TeamViewSnapshot | null | undefined,
-  next: TeamViewSnapshot
-): TeamViewSnapshot {
-  if (!previous) {
-    return next;
-  }
-  return structurallySharePlainValue(previous, next);
 }
 
 const ACTIVE_PROVISIONING_STATES = new Set([
